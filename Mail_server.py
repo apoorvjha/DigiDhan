70b77bf9e63be6474a937160eebefda3c0e5c8e5
apoorvjha <56272803+apoorvjha@users.noreply.github.com>
2020-06-04 15:06:40 +0530

    Create Mail_server.py

---
+++ Mail_server.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+f=open("mail_log.txt","r")
+while(1):
+  server=smtplib.SMTP("localhost",8081)
+	fromaddr="DigiDhan20@gmail.com"
+	k=f.readline()
+	#print(k)
+	if len(k)!=0:
+		q=k.split('~')
+		toaddr=q[0]
+		msg=MIMEMultipart()
+		msg['From']=fromaddr
+		msg['To']=toaddr
+		msg['Subject']=q[1]
+		body=q[2]
+		msg.attach(MIMEText(body,'plain'))
+		text=msg.as_string()
+		server.sendmail(fromaddr,toaddr,text)
+
